9f727dd48064064a1636f124ef8f6126b10d45f3
felipe-augusto142 <felipe.augusto142@gmail.com>
2023-11-03 14:38:45 -0300

    Código funcional

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hi Earth!")
+//Implementação If e Else If
+let XP = 10500
+let nome = "João das Neves"
+let nivel = XP < 1000
+let nivel2 = 1000 <= XP && XP < 2000
+let nivel3 = 2000 <= XP && XP < 3000
+let nivel4 = 3000 <= XP && XP < 4000
+let nivel5 = 4000 <= XP && XP < 5000
+let nivel6 = 5000 <= XP && XP < 6000
+let nivel7 = 6000 <= XP && XP < 7000
+let nivel8 = 7000 <= XP && XP < 8000
+let nivel9 = 8000 <= XP && XP < 9000
+let nivel10 = 9000 <= XP && XP < 10000
+let nivel11 = XP >= 10000
+
+
+if(nivel){
+    titulo="Pedra"
+}else if(nivel2){
+    titulo="Ferro"
+}else if(nivel3){
+    titulo="Cobre"
+}else if(nivel4){
+    titulo="Bronze"
+}else if(nivel5){
+    titulo="Prata"
+}else if(nivel6){
+    titulo="Ouro"
+}else if(nivel7){
+    titulo="Platina"
+}else if(nivel8){
+    titulo="Diamante"
+}else if(nivel9){
+    titulo="Ascendente"
+}else if(nivel10){
+    titulo="Imortal"
+}else if(nivel11){
+    titulo="Radiante"
+}
+
+console.log("O heroi de nome "+nome+" está no nível "+titulo)
+ 
---
+++ index2.js
@@ -0,0 +1,51 @@
+//Implementação Switch
+let XP = 500
+let nome = "João das Neves"
+let titulo = "Madeira"
+
+switch(XP){
+    case XP < 1000:
+    titulo="Pedra"
+    break
+    case 1000 <= XP:
+    case XP < 2000:
+    titulo="Ferro"
+    break
+    case 2000 <= XP:
+    case XP < 3000:
+    titulo="Cobre"
+    break
+    case 3000 <= XP:
+    case XP < 4000:
+    titulo="Bronze"
+    break
+    case 4000 <= XP:
+    case XP < 5000:
+    titulo="Prata"
+    break
+    case 5000 <= XP:
+    case XP < 6000:
+    titulo="Ouro"
+    break
+    case 6000 <= XP:
+    case XP < 7000:
+    titulo="Platina"
+    break
+    case 7000 <= XP:
+    case XP < 8000:
+    titulo="Diamante"
+    break
+    case 8000 <= XP:
+    case XP < 9000:
+    titulo="Ascendente"
+    break
+    case 9000 <= XP:
+    case XP < 10000:
+    titulo="Imortal"
+    break
+    case XP >= 10000:
+    titulo="Radiante"
+}
+
+console.log("O heroi de nome "+nome+" está no nível "+titulo)
+ 
